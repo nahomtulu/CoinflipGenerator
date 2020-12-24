3900390090cd0e122e09fcd43c8f966915f9298b
Nahom Tulu <43589210+nahomtulu@users.noreply.github.com>
2020-12-24 16:19:47 -0600

    First Commit

---
+++ main (5).py
@@ -0,0 +1,19 @@
+#Write your code below this line 👇
+#Hint: Remember to import the random module first. 🎲
+
+import random
+
+random_coinflip = random.randint(0,1)
+
+if random_coinflip == 0:
+  print("Tails")
+else:
+  print("Heads")
+
+
+
+
+
+
+
+
